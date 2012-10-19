a1aa7863f8aa48537e8aad68a7e0779330628ca7
JDrit <jd@csh.rit.edu>
2012-10-19 12:52:52 -0400

    Fixed the parser not to include certain links that are not valid. Links that link to a certain part of another page, now only link to the entire page. Links are stored as a string.

--- parser.py
+++ parser.py
@@ -7,7 +7,7 @@ def readFile(fname):
     f = open(fname) # the xml file to read from
     start = False
     pageTitle = ''  # article's title
-    links = []      # article's links
+    links = ''      # article's links
     entry = []
     count = 0
     
@@ -17,23 +17,22 @@ def readFile(fname):
             start = True
             while '[[' in line:
                 link = line[line.find('[[') + 2 : line.find(']]')]
-                if ':' in link: # dont use link, it is a file or something
-                    pass
-                elif '|' in link: # removes the second part of the link
-                    link = link[:link.find('|')]
-                    links.append(link)
-                else:
-                    links.append(link)
+                if '|' in link: # removes the second part of the link
+                    link = link[:link.find('|')] 
+                if '#' in link: # this removes the href second part of a link
+                    link = link[:link.find('#')]
+                if not ':' in link: # if it has a ':', it is a file or something
+                    links += link + ':'
                 line = line[line.find(']]') + 2:]       
         if '<title>' in line:
             pageTitle = line[11 : -9]
         if '</text>' in line:
             if not len(links) == 1: # one link means it is a redirect
                 count += 1
-                entry = [pageTitle, links]
+                entry = [pageTitle, links.strip(':')]
                 pickle.dump(entry, output)
             start = False
-            links = []
+            links = ''
             entry = []
             if count % 1000 == 0:
                 print(str(count) + " done.")

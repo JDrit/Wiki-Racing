5e276fbeb5890a469e209da73967e6bd083295b5
JDrit <jd@csh.rit.edu>
2012-10-20 00:24:01 -0400

    Changed the parser to make the links a list and make them all lower case so there is no confusion when reading them in the aStar alg.

--- parser.py
+++ parser.py
@@ -7,7 +7,7 @@ def readFile(fname):
     f = open(fname) # the xml file to read from
     start = False
     pageTitle = ''  # article's title
-    links = ''      # article's links
+    links = []      # article's links
     entry = []
     count = 0
     
@@ -22,17 +22,17 @@ def readFile(fname):
                 if '#' in link: # this removes the href second part of a link
                     link = link[:link.find('#')]
                 if not ':' in link: # if it has a ':', it is a file or something
-                    links += link + ':'
+                    links.append(link.lower())
                 line = line[line.find(']]') + 2:]       
         if '<title>' in line:
             pageTitle = line[11 : -9]
         if '</text>' in line:
             if not len(links) == 1: # one link means it is a redirect
                 count += 1
-                entry = [pageTitle, links.strip(':')]
+                entry = [pageTitle, links]
                 pickle.dump(entry, output)
             start = False
-            links = ''
+            links = []
             entry = []
             if count % 1000 == 0:
                 print(str(count) + " done.")
